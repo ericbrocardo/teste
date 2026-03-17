ab1b7a7f863598618c307da81c3e4b563ab8ee5d
eric brocardo <68759993+ericbrocardo@users.noreply.github.com>
2026-03-17 20:15:14 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,18 @@
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        double A, r;
+        r = Integer.parseInt(sc.nextLine());
+
+        A = (r * r) * 3.14159;
+
+        System.out.println("A = " + A);
+
+    }
+}
